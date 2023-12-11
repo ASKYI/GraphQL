f949b6e9e14837c1e10c91e88971ca45379c9f87
Anastasia Iovleva <a.iovleva3@gmail.com>
2023-12-11 16:05:28 +0100

    added a mongoose package and opened DB connection

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
 const schema = require('./schema/schema');
-
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://aiovleva3:AYZwple2mp6pf9IJ@cluster0.bmztq5x.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connection.once('open', () => {
+    console.log("connected to database");
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
